494d95098ae0c59d43946ccdaa0ff5223bb43ace
Pere Tarrida Domingo <58071471+peretarrida13@users.noreply.github.com>
2024-02-15 13:55:35 +0100

    finit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+} );
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
